1298d4e5efafe7b5e5e2ae67216d00ef1e5ac01c
Rostislav Zhuk <zzzhuchok@gmail.com>
2024-01-08 23:30:13 +0300

    refactor(extended-2.1): update CSS according to BEM

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("task-list__item_edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -100,7 +100,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("task-list__item_edit-mode");
 };
 
 
